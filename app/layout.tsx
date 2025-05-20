b4c8d54422a5b101eb1041ef6c5a5311ba450476
Eyaly555 <eyal.y.mi@gmail.com>
2025-05-20 20:16:28 +0300

    hotjar

--- app/layout.tsx
+++ app/layout.tsx
@@ -173,6 +173,21 @@ export default function RootLayout({
             gtag('config', '${GA_MEASUREMENT_ID}');
           `}
         </Script>
+        <Script
+          id="hotjar-init"
+          strategy="afterInteractive"
+        >
+          {`
+            (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:6409846,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          `}
+        </Script>
       </head>
       <body className={`${poppins.variable} ${inter.variable} font-sans antialiased`}>
         <ThemeProvider
